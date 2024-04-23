03497eedf1cffa51ad9296edde94b8304e77f58a
qren <qtter85@gmail.com>
2024-04-23 09:14:16 +0700

    Initial commit

---
+++ main.py
@@ -0,0 +1,81 @@
+import asyncio
+import json
+
+from fastapi import FastAPI
+from pydantic import BaseModel
+from starlette.middleware.cors import CORSMiddleware
+from starlette.websockets import WebSocket
+
+app = FastAPI()
+
+
+reactions_arr = []
+websockets: list[WebSocket] = []
+websockets2: list[WebSocket] = []
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=['*'],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+
+@app.websocket('/reactions')
+async def reactions(websocket: WebSocket):
+    await websocket.accept()
+
+    websockets.append(websocket)
+
+    try:
+        while True:
+            await asyncio.sleep(1)
+
+    except Exception:
+        websockets.remove(websocket)
+        await websocket.close()
+
+
+@app.websocket('/emoji')
+async def reactions(websocket: WebSocket):
+    await websocket.accept()
+
+    websockets2.append(websocket)
+
+    try:
+        while True:
+            await asyncio.sleep(1)
+
+    except Exception:
+        websockets2.remove(websocket)
+        await websocket.close()
+
+
+class Data(BaseModel):
+    text: str
+
+
+@app.post('/reaction')
+async def reaction(data: Data):
+    reactions_arr.append(data.text)
+
+    for ws in websockets:
+        try:
+            await ws.send_text(data.text)
+
+        except:
+            websockets.remove(ws)
+            print('No websocket')
+
+
+@app.post('/emoji')
+async def emoji(data: Data):
+    reactions_arr.append(data.text)
+
+    for ws in websockets2:
+        try:
+            await ws.send_text(data.text)
+
+        except:
+            websockets2.remove(ws)
+            print('No websocket')
